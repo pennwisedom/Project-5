9b025699008fcf6dcbf77c8e6ccf6907017aeacd
Aaron <Pennwisedom@gmail.com>
2020-10-06 16:01:57 -0400

    Post, Likes, WatchList Models created.

--- network/models.py
+++ network/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="poster")
+    timestamp = models.DateField(auto_now_add=True)
+    text = models.CharField(max_length=280)
+
+    def __str__(self):
+        return f"{self.user} made a post at {self.timestamp}."
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ManyToManyField(User, related_name="liker")
+
+    def __str__(self):
+        return f"{self.post.user.username}'s post ({self.post.id}) was liked by {self.user.get().username}."
+
+class WatchList(models.Model):
+    userwatcher = models.ManyToManyField(User, related_name="watchee")
+    userwatchee = models.ManyToManyField(User, related_name="watchers")
+
+    def __str__(self):
+        return f"{self.userwatcher.get().username} is watching {self.userwatchee.get().username}."
